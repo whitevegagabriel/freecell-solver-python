e3608b361d7170d81763bc64335d4fc3b16b7673
Gabriel White-Vega <gabriel.white-vega@gm.com>
2021-07-04 16:50:56 -0400

    Implement next_games_free

--- freecellsolver.py
+++ freecellsolver.py
@@ -1,32 +1,32 @@
 from collections import deque
 
 class GameConstants:
-    solution = [
-        [], # free cells
-        [13, 13, 13, 13], # solution cells
-        [[],
+    solution = {
+        'foundation' : [13, 13, 13, 13],
+        'free' : set(),
+        'cascades' : [[],
          [],
          [],
          [],
          [],
          [],
          [],
-         [],] # columns
-    ]
+         []]
+    }
     solution_str = str(solution)
 
-initial_game = [
-    [], # free cells
-    [13, 13, 13, 12], # solution cells
-    [[],
-     [],
-     [],
-     [],
-     [],
-     [],
-     [],
-     [[13, 3]]] # columns
-]
+initial_game = {
+    'foundation' : [13, 13, 13, 12],
+    'free' : set(),
+    'cascades' : [[],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [[13, 3]]]
+}
 
 #TODO: implement
 def peek_top_stack_el(game, stack_num):
@@ -42,7 +42,7 @@ def put_top_stack_el(game, stack_num, el):
     return game
 
 #TODO: implement
-def eligible_stacks(game, stack_num):
+def eligible_stacks(game, card):
     stack_nums = []
     return stack_nums
 
@@ -58,21 +58,22 @@ def put_free_cell_el(game, el):
 def add_card_to_stacks(game_base, stack_nums, card):
     games = []
     for stack_num in stack_nums:
-        new_game = put_top_stack_el(game_base, stack_num, card)
+        new_game = put_top_stack_el(game_base.copy(), stack_num, card.copy())
         games.append(new_game)
     return games
 
 #TODO: implement
 def add_card_to_solution(game_base, card):
     games = []
+    game_base = game_base.copy()
+    if game_base[0]
     return games
 
-#TODO: implement
-def next_games_stacks(game):
+def next_games_cascades(game):
     games = []
     for i in range(8):
-        stack_nums = eligible_stacks(game, i)
         card_to_move = peek_top_stack_el(game, i)
+        stack_nums = eligible_stacks(game, card_to_move)
         new_game_base = del_top_stack_el(game, i)
         games += add_card_to_stacks(new_game_base, stack_nums, card_to_move)
         games += add_card_to_solution(new_game_base, card_to_move)
@@ -81,15 +82,19 @@ def next_games_stacks(game):
             games.append(new_game)
     return games
 
-#TODO: implement
-def next_games_cells(game):
+def next_games_free(game):
     games = []
+    for card_to_move in game['free']:
+        stack_nums = eligible_stacks(game, card_to_move)
+        new_game_base = game.copy()['free'].remove(card_to_move)
+        games += add_card_to_stacks(new_game_base, stack_nums, card_to_move)
+        games += add_card_to_solution(new_game_base, card_to_move)
     return games
 
 def next_games(game):
     games = []
-    games += next_games_stacks(game)
-    games += next_games_cells(game)
+    games += next_games_cascades(game)
+    games += next_games_free(game)
     return games
 
 #TODO: implement
